d924e019c8a13e1e849e8c0bbde8d91820aa5f16
nikesuputra <wayan.suputra@hostinger.com>
2021-01-20 03:28:22 +0800

    almost finished with deleting posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(20);
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        /* Post::create([
+            'user_id' => auth()->id(),
+            'body' => $request->body
+        ]); */
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
+    }
+
+    public function destroy(Post $post)
+    {
+        if (!$post->ownedBy(auth()->user())) {
+            dd('ok');
+        }
+
+        $post->delete();
+
+        return back();
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,8 +3,9 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}">
-            
+            <form action="{{ route('posts') }}" method="post" class="mb-4">
+                @csrf
+                
                 <div class="mb-4">
                     <label for="body" class="sr-only">Body</label>
                     <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post something!"></textarea>
@@ -21,6 +22,49 @@
                 </div>
             
             </form>
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold">{{ $post->user->name }} <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span></a>
+
+                        <p class="mb-2">{{ $post->body }}</p>
+
+                        @if ($post->ownedBy(auth()->user()))
+                            <div>
+                                <form action="{{ route('posts.destroy', $post) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-blue-500">Delete</button>
+                                </form>
+                            </div>
+                        @endif
+
+                        <div class="flex items-center">
+                            @auth
+                                @if (!$post->likedBy(auth()->user()))
+                                    <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                        @csrf
+                                        <button type="submit" class="text-blue-500">Like</button>
+                                    </form>
+                                @else
+                                    <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-blue-500">Unlike</button>
+                                    </form>
+                                @endif                            
+                            @endauth
+
+                            <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+                        </div>
+                    </div>
+                @endforeach
+
+                {{ $posts->links() }}
+            @else
+                <p>There are no posts</p>
+            @endif
         </div>
     </div>
 @endsection
